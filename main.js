55ecc21d83c50d17232910a2ff9f0a93436207f0
Admin <reanajb@gmail.com>
2023-11-28 12:01:22 -0500

    completed homework

--- main.js
+++ main.js
@@ -2,9 +2,46 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+function isItLong(str) {
+   if(str.length > 20) {
+    return "That's a long string!"
+  }
+}
+
+function isItMedium(str) {
+  if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  }
+}
 
 
+function isItShort(str) {
+  if (str.length < 10) {
+    return "That's a small string!"
+  } else {
+    return "That's not a small string!"
+  }
+}
 
+function howLongIsMyString(str) {
+  if (str.length > 20) {
+    return "That's a long string!"
+  } else if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  } else if (str.length < 10) {
+    return "That's a small string!"
+  }
+}
+
+function personsHeight(str){
+  if(str === "Shaq") {
+    return 85
+  } else if (str === "Zooey") {
+    return 65
+  } else{
+    return "I don't know everyone's height!"
+  }
+}
 /**********************************
  *      OUR CODE GOES BELOW.      *
  * THIS IS FOR INTERNAL USE ONLY. *
